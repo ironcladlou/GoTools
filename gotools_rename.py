6c4485e6d0c5eeb5ff3365f51ac0b0810e68bc7e
Chris Bednarski <banzaimonkey@gmail.com>
2015-09-01 13:33:40 -0700

    Implemented gorename command

--- gotools_rename.py
+++ gotools_rename.py
@@ -14,37 +14,21 @@ class GotoolsRenameCommand(sublime_plugin.TextCommand):
 
   def run(self, edit):
     self.settings = GoToolsSettings()
+    self.settings.debug_enabled = True
     self.logger = Logger(self.settings)
     self.runner = ToolRunner(self.settings, self.logger)
+    self.view.window().show_input_panel("rename", "", self.do_rename, None, None)
 
-    filename, row, col, offset, offset_end = Buffers.location_at_cursor(self.view)
-    pos = "{file}:{row}.{col}".format(file=filename, row=row, col=col)
+  def do_rename(self, name):
+    self.logger.status("running rename")
+    filename, _row, _col, offset, _offset_end = Buffers.location_at_cursor(self.view)
+    args = [
+      "-offset", "{file}:#{offset}".format(file=filename, offset=offset),
+      "-to", name
+    ]
+    output, err, exit = self.runner.run("gorename", args, timeout=15)
 
-
-    sublime.active_window().run_command("hide_panel", {"panel": "output.gotools_rename"})
-
-    # https://www.sublimetext.com/docs/api-reference
-    # showInputPanel(caption, initialText, onDone, onChange, onCancel)
-
-    sublime.set_timeout_async(lambda: self.do_plain_rename("peers", pos, package_scope), 0)
-
-  def do_plain_rename(self, mode, pos, package_scope=[], regex="^(.*):(\d+):(\d+):(.*)$"):
-    self.logger.status("running rename "+mode+"...")
-    args = ["-pos="+pos, "-format=plain", mode]
-    if len(package_scope) > 0:
-      args = args + package_scope
-    output, err, rc = self.runner.run("rename", args, timeout=60)
-    self.logger.log("rename "+mode+" output: " + output.rstrip())
-
-    if rc != 0:
-      self.logger.status("rename call failed (" + str(rc) +")")
+    if exit != 0:
+      self.logger.status("rename failed ({0}): {1}".format(exit, err))
       return
-    self.logger.status("rename "+mode+" finished")
-
-    panel = self.view.window().create_output_panel('gotools_rename')
-    panel.set_scratch(True)
-    panel.settings().set("result_file_regex", regex)
-    panel.run_command("select_all")
-    panel.run_command("right_delete")
-    panel.run_command('append', {'characters': output})
-    self.view.window().run_command("show_panel", {"panel": "output.gotools_rename"})
+    self.logger.status("renamed symbol to {name}".format(name=name))
