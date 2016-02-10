e9cc3dcd157153c1e49044631b17058d2ef5e94e
Dan Mace <ironcladlou@gmail.com>
2016-02-10 08:07:25 -0500

    WIP

--- gotools_format.py
+++ gotools_format.py
@@ -30,17 +30,8 @@ class GotoolsFormat(sublime_plugin.TextCommand):
 
     stdout, stderr, rc = ToolRunner.run(command, args, stdin=Buffers.buffer_text(self.view))
 
-    # Clear previous syntax error marks
-    self.view.erase_regions("mark")
-
-    if rc == 2:
-      # Show syntax errors and bail
-      self.show_syntax_errors(stderr)
-      return
-
     if rc != 0:
-      # Ermmm...
-      Logger.log("unknown gofmt error (" + str(rc) + ") stderr:\n" + stderr)
+      Logger.log("format aborted due to syntax errors (exited {rc})".format(rc=rc))
       return
 
     if GoToolsSettings.get().format_backend == "both":
@@ -48,21 +39,9 @@ class GotoolsFormat(sublime_plugin.TextCommand):
       args = ["-e", "-s"]
       stdout, stderr, rc = ToolRunner.run(command, args, stdin=stdout.encode('utf-8'))
 
-    # Clear previous syntax error marks
-    self.view.erase_regions("mark")
-
-    if rc == 2:
-      # Show syntax errors and bail
-      self.show_syntax_errors(stderr)
-      return
-
     if rc != 0:
-      # Ermmm...
-      Logger.log("unknown gofmt error (" + str(rc) + ") stderr:\n" + stderr)
+      Logger.log("format aborted due to syntax errors (exited {rc})".format(rc=rc))
       return
-
-    # Everything's good, hide the syntax error panel
-    self.view.window().run_command("hide_panel", {"panel": "output.gotools_syntax_errors"})
 
     # Remember the viewport position. When replacing the buffer, Sublime likes to jitter the
     # viewport around for some reason.
@@ -76,30 +55,3 @@ class GotoolsFormat(sublime_plugin.TextCommand):
 
   def restore_viewport(self):
     self.view.set_viewport_position(self.prev_viewport_pos, False)
-
-  # Display an output panel containing the syntax errors, and set gutter marks for each error.
-  def show_syntax_errors(self, stderr):
-    output_view = self.view.window().create_output_panel('gotools_syntax_errors')
-    output_view.set_scratch(True)
-    output_view.settings().set("result_file_regex","^(.*):(\d+):(\d+):(.*)$")
-    output_view.run_command("select_all")
-    output_view.run_command("right_delete")
-
-    syntax_output = stderr.replace("<standard input>", self.view.file_name())
-    output_view.run_command('append', {'characters': syntax_output})
-    self.view.window().run_command("show_panel", {"panel": "output.gotools_syntax_errors"})
-
-    marks = []
-    for error in stderr.splitlines():
-      match = re.match("(.*):(\d+):(\d+):", error)
-      if not match or not match.group(2):
-        Logger.log("skipping unrecognizable error:\n" + error + "\nmatch:" + str(match))
-        continue
-
-      row = int(match.group(2))
-      pt = self.view.text_point(row-1, 0)
-      Logger.log("adding mark at row " + str(row))
-      marks.append(sublime.Region(pt))
-
-    if len(marks) > 0:
-      self.view.add_regions("mark", marks, "mark", "dot", sublime.DRAW_STIPPLED_UNDERLINE | sublime.PERSISTENT)
